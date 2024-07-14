29653862656c3d484a158387cc8783029d138619
ElieneMontenegro <lila.amaral@hotmail.com>
2024-07-14 05:14:58 -0300

    create angular app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,13 +18,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'twh-frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('twh-frontend');
-  });
+  }); 
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'twh-frontend';
+  discordTag: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    console.log("hello world");
+  }
 }
