663b36019f396322d6f2d9846695fb9303e12c40
ElieneMontenegro <lila.amaral@hotmail.com>
2025-03-27 01:53:28 -0300

    environments

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlayerService {
-  private apiUrl = 'http://localhost:8080/docs/names';
+  private apiUrl = environment.backendUrl;
 
   constructor(private http: HttpClient) {}
 
